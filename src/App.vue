99d809fc0b972af2fc81043b66efc6e763f17bd7
vinimarcili <vinipetter@gmail.com>
2017-08-24 09:06:59 -0300

    Define default css application

--- src/App.vue
+++ src/App.vue
@@ -11,14 +11,17 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  background: darken(#fff, 20%);
+  margin: 0 auto;
+  padding: 0;
+  display: block;
+  width: 100%;
+  height: auto;
+  position: relative;
 }
 </style>
